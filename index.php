22cfbd9aab908ffc79494f52cd7a40e6551ca1ba
Jonathan <dancette.jonathan@gmail.com>
2019-10-25 12:58:12 +0200

    Change locations

--- index.php
+++ index.php
@@ -6,18 +6,17 @@ require_once('class/DisplayContent.php');
 
 
 $weather = new OpenWeather('43cebb6f101584f15a47a1581d009ee7');
-$forecast = $weather->getForecast('montgeron,fr', '5');
-$today = $weather->getToday('montgeron, fr');
+$forecast = $weather->getForecast('saclay,fr', '5');
+$today = $weather->getToday('saclay, fr');
 
 
 $cityMapper = new cityMapper('ab74fda8f137038566a7bb1253b533de');
-$travel = $cityMapper->getTimeBetweenTwoPoints('Montgeron', '48.7041762,2.4557377', 'Jouy-en-Josas', '48.7528304,2.1795755');
+$travel = $cityMapper->getTimeBetweenTwoPoints('Gare de Vauboyen', '48.7591376,2.1897711', 'Massy-Palaiseau RER, Massy', '48.7252492,2.2584772');
 
 if(is_null($today) || is_null($forecast) || is_null($travel)) {
     echo 'L\'API d\'OpenWeather ou de CityMapper ne réponds pas :( Revenez plus tard !';
     return '';
 }
-
 
 
 $inputData = [
